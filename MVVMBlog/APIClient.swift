e229b8ad8ad2b943c85c1c2dafe173315663a22e
Erica Millado <ericamillado@gmail.com>
2017-06-16 21:41:19 -0400

    Final

--- MVVMBlog/APIClient.swift
+++ MVVMBlog/APIClient.swift
@@ -8,6 +8,44 @@
 
 import UIKit
 
+//1 - This APIClient will be called by the viewModel to get our top 100 app data.
 class APIClient: NSObject {
+    
+    //2 - the completion handler will be executed after our top 100 app data is fetched
+    // our completion handler will include an optional array of NSDictionaries parsed from our retrieved JSON object
+    func fetchAppList(completion: @escaping ([NSDictionary]?) -> Void) {
+        
+        //3 - unwrap our API endpoint
+        guard let url = URL(string: "https://rss.itunes.apple.com/api/v1/us/ios-apps/top-free/100/explicit/json") else {
+            print("Error unwrapping URL"); return }
+        
+        //4 - create a session and dataTask on that session to get data/response/error
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url) { (data, response, error) in
+            
+            //5 - unwrap our returned data
+            guard let unwrappedData = data else { print("Error getting data"); return }
+            
+            do {
+                //6 - create an object for our JSON data and cast it as a NSDictionary
+                // .allowFragments specifies that the json parser should allow top-level objects that aren't NSArrays or NSDictionaries.
+                if let responseJSON = try JSONSerialization.jsonObject(with: unwrappedData, options: .allowFragments) as? NSDictionary {
+                    
+                    //7 - create an array of dictionaries from
+                    if let apps = responseJSON.value(forKeyPath: "feed.results") as? [NSDictionary] {
+                        
+                        //8 - set the completion handler with our apps array of dictionaries
+                        completion(apps)
+                    }
+                }
+            } catch {
+                //9 - if we have an error, set our completion with nil
+                completion(nil)
+                print("Error getting API data: \(error.localizedDescription)")
+            }
+        }
+        //10 -
+        dataTask.resume()
+    }
 
 }
--- MVVMBlog/ViewModel.swift
+++ MVVMBlog/ViewModel.swift
@@ -8,6 +8,67 @@
 
 import UIKit
 
+//1 - Setup my viewModel that inherits from NSObject
 class ViewModel: NSObject {
 
+    //2 - Create an apiClient property that we can use to call on our API call
+    //This apiClient property is marked as an @IBOutlet so that we can instantiate it from the storyboard.  I mark this with a bang operator (!) since I know it will not be nil since the storyboard will be injecting it. (
+    @IBOutlet var apiClient: APIClient!
+    
+    //3 - Define an apps property that will hold the data from the iTunes RSS top 100 free apps feed
+    //This array is marked an optional (?) because we might not get back data from the iTunes API
+    var apps: [NSDictionary]?
+    
+    //4 - This function is what directly accesses the apiClient to make the API call
+    func getApps(completion: @escaping () -> Void) {
+        
+        //5 - call on the apiClient to fetch the apps
+        apiClient.fetchAppList { (arrayOfAppsDictionaries) in
+            
+            //6 - Put this block on the main queue because our completion handler is where the data display code will happen and we don't want to block any UI code.
+            DispatchQueue.main.async {
+                
+                //7 - assign our local apps array to our returned json
+                self.apps = arrayOfAppsDictionaries
+                
+                //8 - call our completion handler because it is in this completion that we will reload data in our view controller tableview
+                completion()
+            }
+        }
+    }
+    
+    // MARK: - values to display in our table view controller
+    //9 -
+    func numberOfItemsToDisplay(in section: Int) -> Int {
+        return apps?.count ?? 0
+    }
+    
+    //10 -
+    func appTitleToDisplay(for indexPath: IndexPath) -> String {
+        return apps?[indexPath.row].value(forKeyPath: "name") as? String ?? ""
+    }
+    
+    //11 -
+    func appRatingToDisplay(for indexPath: IndexPath) -> String {
+        return apps?[indexPath.row].value(forKeyPath: "contentAdvisoryRating") as? String ?? ""
+    }
+    
+    
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
