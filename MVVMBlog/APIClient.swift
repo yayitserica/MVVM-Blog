8bfca1a5e802230cdbbc0e56365238cea761ea1f
Erica Millado <ericamillado@gmail.com>
2017-06-16 21:41:08 -0400

    first commit

---
+++ MVVMBlog/APIClient.swift
@@ -0,0 +1,13 @@
+//
+//  APIClient.swift
+//  MVVMBlog
+//
+//  Created by Erica Millado on 6/16/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
+//
+
+import UIKit
+
+class APIClient: NSObject {
+
+}
---
+++ MVVMBlog/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  MVVMBlog
+//
+//  Created by Erica Millado on 6/16/17.
+//  Copyright © 2017 Erica Millado. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
